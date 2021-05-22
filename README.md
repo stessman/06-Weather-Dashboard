b5f7e49efb0c5139f1e2a1d78e3663a1002d005d
Scott T <scott.tessm@gmail.com>
2021-05-22 13:30:50 -0500

    Finished up js functionality  of the assignment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# 06-Weather-Dashboard
+# Unit 06 Homework : Weather-Dashboard
+
+This is the homework assignment for Unit 06 Server Side API's
+
+<img src="assets\img\weatherDashboard.PNG">
+
+## What problem are we trying to solve
+This project was created as a way for people to track and alter their daily activities in a schedule format. When the user opens the scheduler they are presented at the top of the page with today's day of the week, month, and day. As they scroll down the page they are presented with timeblocks for common work hours. These timeblocks are color coded as grey for past hours of the day, red for the current hour, and green for future hours of the day. The user is able to enter text into the timeblocks and click on the corresponding save button to save that information for that hour. To clear out the text in a given hour the user must erase all of the text in the timeblock and then click the save button. The saved information on the scheduler will persist on page load and page reload.
+
+## Requirements
+- When the user opens the planner then the current day is displayed at the top of the calendar.
+- When the user scrolls down the page then they are presented with timeblocks for standard business hours.
+- When the user views the timeblocks for that day then each timeblock is color coded to indicate whether it is in the past, present, or future.
+- When the user clicks into a timeblock then they can enter an event.
+- When the user clicks on the save button for that time timeblock then the text for that event is saved in local storage.
+- When the user refreshes the page then the saved events persist.
+
+## Notes
+I believe that this project went very well. The functionality of my code fits the requirements of this task. There may be some nicer ways of selecting elements and traversing the DOM than what I did in my functions, but overall everything works as expected with no errors.
+
+Link to the live site: [Work Day Scheduler](https://stessman.github.io/05-Work-Day-Scheduler/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,15 @@ let currentLocationLon;
 let currentLocationLat;
 let citySearchCity;
 let DateTime = luxon.DateTime;
+let indexText;
+let iconLink;
 let todaysDate;
 let tomorrow;
 
-
+// Gets the city name and the longitude and latitude of the searched city to be used later
 function citySearchButtonHandler () {
     citySearchCity = citySearchField.value;
+    citySearchField.value = '';
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + citySearchCity + '&appid=3290fa4ab7af532d7cd9308947e66b17&units=imperial')
         .then(function (response) {
         return response.json();
@@ -27,28 +30,39 @@ function citySearchButtonHandler () {
         currentLocation = data.name;
         currentLocationLat = data.coord.lat;
         currentLocationLon = data.coord.lon;
-        locationName.innerHTML = data.name;
-        //currentLocationIcon.setAttribute("class", data.weather[0].icon);
+        iconLink = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
         displayCurrentLocation();
         addCityToRecentSearch(currentLocation, currentLocationLat, currentLocationLon);
     });
 }
 
+// Displays the weather information for the current searched location
 function displayCurrentLocation () {
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + currentLocationLat + '&lon=' + currentLocationLon + '&appid=3290fa4ab7af532d7cd9308947e66b17&units=imperial&exclude=minutely,hourly,alerts')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
         todaysDate = data.current.dt;
         todaysDate = DateTime.fromSeconds(todaysDate);
         todaysDate = todaysDate.toLocaleString(DateTime.DATE_SHORT);
-        locationName.innerHTML = locationName.innerHTML + ' ' + todaysDate;
+        locationName.innerHTML = currentLocation + ' ' + todaysDate;
+        let newIcon = document.createElement("img");
+        newIcon.setAttribute("src", iconLink)
+        locationName.appendChild(newIcon);
         locationsCurrentTemp.innerHTML = "Temp: " + data.current.temp;
         locationsCurrentWind.innerHTML = "Wind: " + data.current.wind_speed + ' MPH';
         locationsCurrentHumidity.innerHTML = "Humidity: " + data.current.humidity + ' %';
-        locationsCurrentUVIndex.innerHTML = "UV Index: " + data.current.uvi;
+        if(locationsCurrentUVIndex.hasChildNodes()){
+            locationsCurrentUVIndex.removeChild(locationsCurrentUVIndex.firstChild);
+        }
+        locationsCurrentUVIndex.innerHTML = "UV Index: ";
+        indexText = document.createElement("SPAN");
+        indexText.setAttribute("id", "indexText");
+        let currentIndex = Number(data.current.uvi);
+        indexText.innerHTML = currentIndex;
+        checkIndex(currentIndex);
+        locationsCurrentUVIndex.appendChild(indexText);
         fillFiveDayForecast(data);
     });
 }
@@ -56,75 +70,107 @@ function displayCurrentLocation () {
 // Fills the 5-Day Forecast area with data
 function fillFiveDayForecast (data) {
     let j = 0;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 1; i < 6; i++) {
         let forecastDay = data.daily[i].dt;
         forecastDay = DateTime.fromSeconds(forecastDay);
         forecastDay = forecastDay.toLocaleString(DateTime.DATE_SHORT);
         dailyForecastSections[j].children[0].innerHTML = forecastDay;
-        //dailyForecastSections[j].children[1].innerHTML    NEED TO DO THIS IS FOR ICONS
-        dailyForecastSections[j].children[2].innerHTML = data.daily[i].temp.day + ' ' + '<span>&#176;</span>' + 'F';
-        dailyForecastSections[j].children[3].innerHTML = data.daily[i].wind_speed + " MPH";
-        dailyForecastSections[j].children[4].innerHTML = data.daily[i].humidity + " %";
+        iconLink = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png";
+        dailyForecastSections[j].children[1].src = iconLink;
+        dailyForecastSections[j].children[2].innerHTML = "Temp: " + data.daily[i].temp.day + ' ' + '<span>&#176;</span>' + 'F';
+        dailyForecastSections[j].children[3].innerHTML = "Wind: " + data.daily[i].wind_speed + " MPH";
+        dailyForecastSections[j].children[4].innerHTML = "Humidity: " +data.daily[i].humidity + " %";
         j++;
     }
 }
 
+// When a city is searched it is then added to the recent search data
 function addCityToRecentSearch (cityName, cityLat, cityLon) {
-    console.log(currentLocation);
-    //if (localStorage.getItem(""))
-    //for keys in 
     if (localStorage.getItem('PreviousCities') === null){
         let citiesArray = [];
         let citiesObject = {};
         let item = {
             "City": cityName,
-            "Lon": cityLat,
-            "Lat": cityLon
+            "Lon": cityLon,
+            "Lat": cityLat
         }
         citiesObject.items = citiesArray;
         citiesObject.items.unshift(item);
         localStorage.setItem('PreviousCities', JSON.stringify(citiesObject));
     }else{
         citiesObject = JSON.parse(localStorage.getItem("PreviousCities"));
+        for (let i = 0; i < citiesObject.items.length; i++){
+            if (citiesObject.items[i].City === cityName) {
+                citiesObject.items.splice(i,1);
+            }
+        }
+        if(citiesObject.items.length > 7) {
+            citiesObject.items.pop();
+        }
         let item = {
             "City": cityName,
-            "Lon": cityLat,
-            "Lat": cityLon
+            "Lon": cityLon,
+            "Lat": cityLat
         }
-        //citiesObject.items = citiesArray;
         citiesObject.items.unshift(item);
         localStorage.setItem('PreviousCities', JSON.stringify(citiesObject));
     }
     createButtons();
 }
 
-
+// Creates the buttons on the page for the Recent Searches Area
 function createButtons () {
+    while (pastSearchArea.firstChild){
+        pastSearchArea.removeChild(pastSearchArea.firstChild);
+    }
     if(localStorage.getItem("PreviousCities")!== null) {
         let recentCitiesList = JSON.parse(localStorage.getItem("PreviousCities"));
         for (let i = 0; i < recentCitiesList.items.length; i++){
             let newButton = document.createElement("BUTTON");
             newButton.innerHTML = recentCitiesList.items[i].City;
             newButton.setAttribute("type", "button");
-            newButton.setAttribute("class", "btn btn-primary");
+            newButton.setAttribute("class", "btn btn-primary recentBtn");
             pastSearchArea.appendChild(newButton);
         }
     }
 }
 
 
+// Handles when a button is clicked in the Previous Search Area
+function handlePastSearchAreaClick (event) {
+    event.preventDefault();
+    let newLocation = event.target;
+    if (newLocation.nodeName !== "BUTTON"){
+        return;
+    }
+    newLocation = newLocation.innerHTML;
+    let recentCitiesList = JSON.parse(localStorage.getItem("PreviousCities"));
+    for (let i = 0; i < recentCitiesList.items.length; i++){
+        if (recentCitiesList.items[i].City === newLocation) {
+            currentLocation = recentCitiesList.items[i].City;
+            currentLocationLat = recentCitiesList.items[i].Lat;
+            currentLocationLon = recentCitiesList.items[i].Lon;
+            displayCurrentLocation();
+        }
+}
+}
 
+//Checks the UV Index and sets the color background of it
+function checkIndex (uvIndex) {
+    if(uvIndex < 3) {
+        indexText.style.backgroundColor = "green";
+    } else if (3 <= uvIndex && uvIndex < 6) {
+        indexText.style.backgroundColor = "yellow";
+    } else if (6 <= uvIndex && uvIndex < 8) {
+        indexText.style.backgroundColor = "orange";
+    } else if (8 <= uvIndex && uvIndex < 11) {
+        indexText.style.backgroundColor = "red";
+    } else if (11 <= uvIndex) {
+        indexText.style.backgroundColor = "violet";
+    } 
+    indexText.style.color = "white";
+}
+
+createButtons();
+pastSearchArea.addEventListener("click", handlePastSearchAreaClick);
 citySearchButton.addEventListener("click", citySearchButtonHandler);
-
-/*
-Store recent cities into local storage
-When a city is searched, check to see if it is already added to the list, else add it to the top of the list
-If the list is length of 8, then drop the item at 8th spot
-For each item in the local storage list create a button in the recent search area
-
-click handle
-
-when a recent search is clicked then pull data from local storage and search for that city
-
-Nice to have: If city doesn't exist
-*/
